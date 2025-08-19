2cc52ed197a759b578075f051e8ed02e038eb79b
sumit45 <sumit619.hellboy@gmail.com>
2025-08-19 09:36:15 +0530

    Creating Two Classes

---
+++ src/test/java/dummyScript2/ToExecuteDummy2.java
@@ -0,0 +1,11 @@
+package dummyScript2;
+
+public class ToExecuteDummy2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Smoke Suite Executed");
+	}
+
+}
